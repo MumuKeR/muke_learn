a62b44cafd3f21efeea45beda89f9e6a60b48f79
K <13207161269@qq.com>
2020-11-07 01:16:30 +0800

    创建vue项目

--- JavaSourceLearn/src/Main.java
+++ JavaSourceLearn/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        try {
+            throw new RuntimeException("异常");
+        } catch (RuntimeException e) {
+            e.getMessage();
+        }
+        list.stream().forEach(i -> {
+            System.out.println("i = " + i);
+        });
     }
+
+
+
 }
